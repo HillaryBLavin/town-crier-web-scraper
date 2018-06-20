885a26f74c42e0bac5e53a1407d6f426a9e81941
Hillary Lavin <hilavin@gmail.com>
2018-06-20 17:27:28 -0400

    Add dependency req, db req, express init, body-parser, express static

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
+// Require Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+// var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
