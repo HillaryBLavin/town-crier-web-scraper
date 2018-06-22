fc8de30289787536e5de6b844736bb3cef37218b
Hillary Lavin <hilavin@gmail.com>
2018-06-22 14:58:54 -0400

    Import requirements, write GET for fetch method

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,11 @@
+// Import scrape script
+var scrape = require("../scripts/scrape");
+
+// Import controllers
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
+
+
 // Routes for GET requests to render site pages
 module.exports = function(router) {
     // Render Home page
@@ -8,4 +16,18 @@ module.exports = function(router) {
     router.get("/saved", function (req, res) {  
         res. render("saved");
     });
-}
+
+    router.get("/api/fetch", function (req, res) {
+        headlinesController.fetch(function (err, docs) {
+            if (!docs || docs.insertedCount === 0) {
+                res.json({
+                    message: "All's quiet, m'lord. Check back on the morrow!"
+                });
+            } else {
+                res.json({
+                    message: "Hear ye, hear ye! " + docs.insertedCount + " new articles!"
+                });
+            }
+        });
+    });
+} 
