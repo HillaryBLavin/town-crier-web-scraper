d6af49ac15031666e1dd63f0144369e1942fb45b
Hillary Lavin <hilavin@gmail.com>
2018-06-25 13:04:34 -0400

    Edit javascript for UI display

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-    var articleContainer = $(".article-container"); // Hosaved into article-container div
+    var articleContainer = $(".article-container"); // Grab onto article-container div
     $(document).on("click", ".btn.save", handleArticleSave); // On-click for "Save Article" button
     $(document).on("click", ".scrape-new", handleArticleScrape); // On-click for "Scrape Ye New Articles" button
 
-    renderPage(); 
+    initPage(); 
 
     // A function for initially running the page
-    function renderPage() {
-
+    function initPage() {
+        articleContainer.empty();
         $.get("/api/headlines?saved=false")
         .then(function(data) {
-            articleContainer.empty();
             if (data && data.length) {
                 renderArticles(data);
             } else {
@@ -28,38 +27,60 @@ $(document).ready(function() {
         articleContainer.append(articleCards);
     }
 
+
     function createCard(article) {
         var card = 
-            $(
-                `<div class="row">
-                    <div class="col-3">
-                        <img src="${article.thumbnail}" alt="Image" class="img-thumbnail">
-                    </div>
-                    <div class="col-sm-9">
-                        <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">${article.headline}</h5>
-                            <p class="card-text">${article.date}</p>
-                            <a class="btn btn-primary save">Save Article</a>
-                        </div>
-                        </div>
-                    </div>
-                </div>`
-            );
-        card.data("_id", article._id);
+        $(["<div class='card'>",
+            "<h5 class='card-title'>",
+            article.headline,
+            "<a class='btn btn-primary save'>",
+            "Save Article",
+            "</a>",
+            "</h5>",
+            "<div class='card-body'>",
+            "<p class='card-tex'>",
+            article.summary,
+            "</p>",
+            "</div",
+            "</div>"
+        ].join(""));
 
+        card.data("_id", article._id);
         return card;
     }
+    // Attempt at Template Literal
+    // function createCard(article) {
+    //     var card = 
+    //         $(
+    //             `<div class="row">
+    //                 <div class="col-3">
+    //                     <img src="${article.thumbnail}" alt="Image" class="img-thumbnail">
+    //                 </div>
+    //                 <div class="col-sm-9">
+    //                     <div class="card">
+    //                     <div class="card-body">
+    //                         <h5 class="card-title">${article.headline}</h5>
+    //                         <p class="card-text">${article.date}</p>
+    //                         <a class="btn btn-primary save">Save Article</a>
+    //                     </div>
+    //                     </div>
+    //                 </div>
+    //             </div>`
+    //         );
+    //     card.data("_id", article._id);
+
+    //     return card;
+    // }
 
     function renderEmpty() {
         var emptyAlert = $(
         [
             "<div class='alert alert-warning text-center'>",
-            "<h4>Uh Oh. Losaveds like we don't have any new articles.</h4>",
+            "<h4>Forsooth, it appears we don't have any new articles.</h4>",
             "</div>",
             "<div class='card'>",
             "<div class='card-header text-center'>",
-            "<h3>What Would You Like To Do?</h3>",
+            "<h3>What Wouldst Thou Do?</h3>",
             "</div>",
             "<div class='card-body text-center'>",
             "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
@@ -83,7 +104,7 @@ $(document).ready(function() {
         })
         .then(function (data) {  
             if (data.saved) {
-                renderPage();
+                initPage();
             }
         });
     }
@@ -92,7 +113,7 @@ $(document).ready(function() {
 
         $.get("/api/fetch")
             .then(function(data) {
-                renderPage();
+                initPage();
                 bootbox.alert("<h3 class='text-center m-top-80'>" + data.message + "</h3>")
             });
     }
